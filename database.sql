fd2e818adcc266fca5c4e74ceb0cacd2770165a2
Naif <nhast603@mail.rmu.edu>
2026-02-23 00:07:18 -0500

    Add/update database.sql

--- database.sql
+++ database.sql
@@ -1,19 +1,14 @@
-#database creation
-
-CREATE DATABASE electronic_voting;
+CREATE DATABASE IF NOT EXISTS electronic_voting;
 USE electronic_voting;
-
-#Users table
 
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
-    hashed_national_id CHAR(64) NOT NULL,  -- SHA-256
+    hashed_national_id CHAR(64) NOT NULL,
     voting_district ENUM('DistrictA','DistrictB') NOT NULL,
-    public_key TEXT NOT NULL,               -- Public Key 
+    public_key TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-# Elections table
 CREATE TABLE elections (
     election_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -24,7 +19,6 @@ CREATE TABLE elections (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-#Candidates table
 CREATE TABLE candidates (
     candidate_id INT AUTO_INCREMENT PRIMARY KEY,
     election_id INT NOT NULL,
@@ -33,18 +27,11 @@ CREATE TABLE candidates (
     FOREIGN KEY (election_id) REFERENCES elections(election_id) ON DELETE CASCADE
 );
 
-# Voters table
 CREATE TABLE votes (
     vote_id INT AUTO_INCREMENT PRIMARY KEY,
     election_id INT NOT NULL,
-    hashed_ballot CHAR(64) NOT NULL,  -- SHA-256 ballot
-    ppk_hash CHAR(64) NOT NULL,       -- SHA-256 PPK file
+    hashed_ballot CHAR(64) NOT NULL,
+    ppk_hash CHAR(64) NOT NULL,
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (election_id) REFERENCES elections(election_id) ON DELETE CASCADE
 );
-
-
-CREATE INDEX idx_votes_election ON votes(election_id);
-CREATE INDEX idx_users_district ON users(voting_district);
-
-ALTER TABLE votes ADD UNIQUE (election_id, ppk_hash);
